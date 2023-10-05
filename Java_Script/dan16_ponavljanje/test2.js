542784a7d3ba8a94d32c26d599159fa8d8ce4a46
auxilum <haribomedvjedic@gmail.com>
2023-10-05 20:19:21 +0200

    zadatak2

---
+++ Java_Script/dan16_ponavljanje/test2.js
@@ -0,0 +1,7 @@
+//Deklarirajte varijable y i z te im dodijelite proizvoljne vrijednosti. Nakon toga razlomite ovu ternarnu operaciju na 3 različite operacije (upotrebom grananja). Ispišite dobivenu vrijednost varijable x.
+let y = 2;
+let z = 2;
+
+var x = z === 2 ? y : 5;
+
+console.log(x);
